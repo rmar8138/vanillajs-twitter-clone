8f1840c9ccc6a728ccbd1ca2d67b121513cdde35
rmar8138 <ragan.martinez@live.com>
2019-11-26 12:21:27 +1100

    Allow user to add tweets, but only for themselves

--- js/app.js
+++ js/app.js
@@ -1,17 +1,44 @@
 const url = "http://localhost:3000/";
-let tweets = axios
-  .get(url + "tweets")
-  .then(({ data }) => {
-    let tweetList = document.querySelector("#tweetList");
-    data.forEach(tweet => {
-      let html = `
-    <li class="tweet">
-      <span class="tweet__author">${tweet.author}</span>
-      <p class="tweet__body">${tweet.body}</p>
-    </li>
-  `;
 
-      tweetList.insertAdjacentHTML("beforeend", html);
-    });
-  })
-  .catch(error => console.log(error));
+const getTweets = () => {
+  axios
+    .get(url + "tweets")
+    .then(({ data }) => {
+      let tweetList = document.querySelector("#tweetList");
+      data
+        .sort((a, b) => b.timestamp - a.timestamp)
+        .forEach(tweet => {
+          let html = `
+      <li class="tweet">
+        <span class="tweet__author">${tweet.author}</span>
+        <p class="tweet__body">${tweet.body}</p>
+      </li>
+    `;
+
+          tweetList.insertAdjacentHTML("beforeend", html);
+        });
+    })
+    .catch(error => console.log(error));
+};
+
+getTweets();
+
+let tweetForm = document.querySelector("#tweetForm");
+let tweetBody = document.querySelector("#tweetBody");
+
+tweetForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  let body = event.target.elements.tweetBody.value;
+  let author = "Me"; //change this
+  let timestamp = Date.now();
+
+  axios
+    .post(url + "tweets", {
+      body,
+      author,
+      timestamp
+    })
+    .then(({ data }) => console.log(data))
+    .catch(error => console.log(error));
+});
